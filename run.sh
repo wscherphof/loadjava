209ca4f946ecf30ddacf853a856e9450729634f7
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2014-09-23 12:30:14 +0200

    run testlj instead of bash

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker build -t lj .
 docker run --name lj lj true
-docker run --privileged --rm --volumes-from lj wscherphof/oracle-12c /bin/bash -c "/lj/testlj"
+docker run --privileged --rm --volumes-from lj wscherphof/oracle-12c /lj/testlj
 docker rm -v lj
 docker rmi lj
